ef7b8c393eac33d62a1d83cf04eceab33754e7de
Gulshan Soni <43094983+shiv30310@users.noreply.github.com>
2023-06-20 16:46:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,13 @@
-function getFormvalue() {
-    //Write your code here
+const formEl = document.getElementById('form1')
 
+formEl.addEventListener('submit', getFormvalue)
+
+
+
+function getFormvalue(e) {
+  e.preventDefault()
+  const firstName = formEl.elements.fname.value
+  const lastName = formEl.elements.lname.value
+
+  alert(`${firstName} ${lastName}`)
 }
